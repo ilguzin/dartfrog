4876e11f679440c4da8f1cfeea153122d0a32c48
Denis Ilguzin <352586+ilguzin@users.noreply.github.com>
2020-07-16 09:33:55 +0300

    cd:dd-swarm-b:update

--- deploy/.images.sh
+++ deploy/.images.sh
@@ -3,4 +3,4 @@
 # NOTE! Keep version list up to date with latest services versions and development release config file
 
 # image versions
-export DD_AGENT_VERSION='= 7'
+export DD_AGENT_VERSION='= latest-jmx'
