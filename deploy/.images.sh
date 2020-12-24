46ca443aa294244d6ff9f47be33dc744a8fa6a46
Denis Ilguzin <352586+ilguzin@users.noreply.github.com>
2020-12-24 21:23:25 +0300

    Update .images.sh

--- deploy/.images.sh
+++ deploy/.images.sh
@@ -3,5 +3,5 @@
 # NOTE! Keep version list up to date with latest services versions and development release config file
 
 # image versions
-export DD_AGENT_VERSION='= latest'
-# export DD_AGENT_VERSION='= latest-jmx'
+# export DD_AGENT_VERSION='= latest'
+export DD_AGENT_VERSION='= latest-jmx'
